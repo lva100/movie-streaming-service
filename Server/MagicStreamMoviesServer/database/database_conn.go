18c4b9522d65d55da6de7d8281aeedbd3f6e6c17
lva100 <lva100@yandex.ru>
2026-02-27 14:51:19 +0300

    Add User Controller and fixed Movie Controller

--- Server/MagicStreamMoviesServer/database/database_conn.go
+++ Server/MagicStreamMoviesServer/database/database_conn.go
@@ -10,7 +10,34 @@ import (
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-func DBInstance() *mongo.Client {
+func Connect() *mongo.Client {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("Warning: unable to fund .env file")
+	}
+
+	MongoDb := os.Getenv("MONGODB_URI")
+
+	if MongoDb == "" {
+		log.Fatal("MONGODB_URI not set!")
+	}
+
+	fmt.Println("MongoDB URI: ", MongoDb)
+
+	clientOptions := options.Client().ApplyURI(MongoDb)
+
+	client, err := mongo.Connect(clientOptions)
+
+	if err != nil {
+		return nil
+	}
+
+	return client
+}
+
+/*func DBInstance() *mongo.Client {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Println("Warning: unable to fund .env file")
@@ -30,9 +57,29 @@ func DBInstance() *mongo.Client {
 	return client
 }
 
-var Client *mongo.Client = DBInstance()
+var Client *mongo.Client = DBInstance()*/
 
-func OpenCollection(collectionName string) *mongo.Collection {
+func OpenCollection(collectionName string, client *mongo.Client) *mongo.Collection {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Warning: unable to find .env file")
+	}
+
+	databaseName := os.Getenv("DATABASE_NAME")
+
+	fmt.Println("DATABASE_NAME: ", databaseName)
+
+	collection := client.Database(databaseName).Collection(collectionName)
+
+	if collection == nil {
+		return nil
+	}
+	return collection
+
+}
+
+/*func OpenCollection(collectionName string) *mongo.Collection {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Println("Warning: unable to fund .env file")
@@ -49,4 +96,4 @@ func OpenCollection(collectionName string) *mongo.Collection {
 		return nil
 	}
 	return collection
-}
+}*/
--- Server/MagicStreamMoviesServer/main.go
+++ Server/MagicStreamMoviesServer/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/lva100/movie-streaming-service/Server/MagicStreamMoviesServer/controllers"
+	"github.com/lva100/movie-streaming-service/Server/MagicStreamMoviesServer/database"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 func main() {
@@ -14,9 +19,27 @@ func main() {
 		c.String(200, "Hello, MagicStreamMovies!")
 	})
 
-	router.GET("/movies", controllers.GetMovies())
-	router.GET("/movie/:imdb_id", controllers.GetMovie())
-	router.POST("/addmovie", controllers.AddMovie())
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Warning: unable to find .env file")
+	}
+
+	var client *mongo.Client = database.Connect()
+
+	if err := client.Ping(context.Background(), nil); err != nil {
+		log.Fatalf("Failed to reach server: %v", err)
+	}
+	defer func() {
+		err := client.Disconnect(context.Background())
+		if err != nil {
+			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
+		}
+
+	}()
+
+	router.GET("/movies", controllers.GetMovies(client))
+	router.GET("/movie/:imdb_id", controllers.GetMovie(client))
+	router.POST("/addmovie", controllers.AddMovie(client))
 
 	if err := router.Run(":8088"); err != nil {
 		fmt.Println("Failed to start server", err)
